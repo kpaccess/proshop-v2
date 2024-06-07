e0ae9d56cbd51a5531a96cb23044300b7b83762e
krishna pradhan <kpaccess@gmail.com>
2024-06-07 11:31:32 -0400

    display product using card

---
+++ frontend/src/types.ts
@@ -0,0 +1,16 @@
+export type ProductsProps = {
+  _id?: string;
+  name: string;
+  image: string;
+  description: string;
+  brand: string;
+  category: string;
+  price: number;
+  countInStock: number;
+  rating: number;
+  numReviews: number;
+};
+
+export type ProductProps = {
+  product: ProductsProps;
+};
