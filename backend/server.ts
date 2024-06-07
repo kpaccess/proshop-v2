92c297a999e2448be4493a254ba80e8f678c5f95
krishna pradhan <kpaccess@gmail.com>
2024-06-07 17:10:42 -0400

    added concurrently npm to run both client and server from the same package

--- backend/server.ts
+++ backend/server.ts
@@ -1,1 +1,20 @@
-console.log("hello")
+import express from "express";
+
+import products from "./data/products";
+
+const app = express();
+
+const port = 3001;
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
