1db56c5f6563fc5fadaddf9bffec0d809ca6c8de
krishna pradhan <kpaccess@gmail.com>
2024-06-10 11:17:57 -0400

    get products from Database

--- backend/server.ts
+++ backend/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db";
+import productRoutes from "./routes/productRoutes";
 import products from "./data/products";
 
 connectDB();
@@ -9,16 +10,8 @@ const app = express();
 
 const port = process.env.PORT || 3001;
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-// app.get("/api/products/:id", (req, res) => {
-//   const product = products.find((p) => p._id === req.params.id);
-//   res.json(product);
-// });
-// console.log(`$err`, chalk.red.inverse);
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+app.use("/api/products", productRoutes);
